e4624b51bb1717a62b70cf49f4e394305b1ff262
Florent Beaurain <beaurain.florent@protonmail.com>
2018-03-18 22:06:09 +0100

    rework routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,14 @@
 Rails.application.routes.draw do
-  constraints subdomain: 'admin' do
-    devise_for :admins
+  devise_for :admins
+  devise_for :technicians
 
-    scope module: 'admin' do
-      resources :taps
-      resources :tap_templates
-      resources :industrial_units
-    end
-
-    get '/', to: 'admin/industrial_units#index'
+  scope 'admin' do
+    resources :taps
+    resources :tap_templates
+    resources :industrial_units
   end
 
-  constraints subdomain: 'technician' do
-    devise_for :technicians
-
-    scope module: 'technician' do
-    end
-
-    get '/', to: 'technician/taps#index'
+  namespace :technician do
   end
 
   root 'admin/industrial_units#index'
