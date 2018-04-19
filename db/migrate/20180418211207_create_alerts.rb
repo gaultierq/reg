597175354e4a12a4c997c401439e579b70eed1a3
Miyabimaru <gregoire.dubelloy@epitech.eu>
2018-04-19 16:29:43 +0200

    Fix migration alert

--- db/migrate/20180418211207_create_alerts.rb
+++ db/migrate/20180418211207_create_alerts.rb
@@ -2,7 +2,7 @@ class CreateAlerts < ActiveRecord::Migration[5.2]
   def change
     create_table :alerts do |t|
       t.references :industrial_unit, foreign_key: true
-      t.references :tap, foreign_key: true
+      t.references :faucet, foreign_key: true
       t.integer :kind, null: false, default: 0
       t.date :date
 
