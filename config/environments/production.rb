cd85d08f84ddb77f7ee7eeca95e10d2eaab0c826
Miyabimaru <gregoire.dubelloy@epitech.eu>
2018-04-19 18:46:25 +0200

    Try fix css not loading

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,12 +26,10 @@ Rails.application.configure do
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = Uglifier.new(harmony: true)
-  # config.assets.css_compressor = :sass
+  config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
-  config.assets.digest = true
-  config.serve_static_assets = true
+  config.assets.compile = false
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
