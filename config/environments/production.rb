1882c83101b0d01878cbd911b917c8e0fe404386
Miyabimaru <gregoire.dubelloy@epitech.eu>
2018-04-26 14:31:00 +0200

    Try fix mail sender

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,6 +68,17 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
 
   config.action_mailer.default_url_options = { host: 'staging-reg.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.smtp_settings = {
+      :user_name => 'smtpmailsendertest@gmail.com',
+      :password => 'Pass123!',
+      :domain => 'staging-reg.herokuapp.com',
+      :address => 'smtp.gmail.com',
+      :port => 587,
+      :authentication => :plain,
+      :enable_starttls_auto => true
+  }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
