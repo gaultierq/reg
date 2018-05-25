0dc097166c6a720aaaa9773911602803ce86a28e
Miyabimaru <gregoire.dubelloy@epitech.eu>
2018-05-25 14:36:51 +0200

    User faucets link to show when entering RFID number

--- app/controllers/user/faucets_controller.rb
+++ app/controllers/user/faucets_controller.rb
@@ -3,7 +3,12 @@ class User::FaucetsController < User::BaseController
 
   # GET /user/faucets
   def index
-    @faucets = current_user.faucets.all
+    if params[:rfid_number].present?
+      @faucet = Faucet.find_by(rfid_number: params[:rfid_number])
+      redirect_to user_faucet_path(@faucet)
+    else
+      @faucets = current_user.faucets.all
+    end
   end
 
   # GET /user/faucets/1
