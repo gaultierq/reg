6d0b9ba0a4d8fd0340a1ae34791a2d89837b540c
Miyabimaru <gregoire.dubelloy@epitech.eu>
2018-06-11 12:34:44 +0200

    Update GED upload and attachment kinds

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -6,5 +6,9 @@ class Attachment < ApplicationRecord
   has_many :tap_template_attachments
   has_many :tap_templates, through: :tap_template_attachments, dependent: :destroy
 
-  enum kind: { maintenance: 0 }
+  enum kind: { instruction_service: 0,
+               instrumentation_position: 1,
+               instrumentation_pilotage: 2,
+               instrumentation_autre: 3,
+               actionnement_actionneur: 4 }
 end
