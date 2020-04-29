0425502a20cafe0f82de018009a9d06c85c76dac
Quentin Gaultier <quentin.gaultier@gmail.com>
2020-04-29 17:44:52 -0300

    private files

---
+++ app/controllers/active_storage/base_controller.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+# The base controller for all ActiveStorage controllers.
+class ActiveStorage::BaseController < ActionController::Base
+  protect_from_forgery with: :exception
+  before_action  :authenticate!
+
+  before_action do
+    ActiveStorage::Current.host = request.base_url
+  end
+
+  # TODO: check if the document belongs to the user / industrial unit
+  def authenticate!
+    unless user_signed_in? || admin_signed_in?
+      render :file => "#{Rails.root}/public/404.html",  layout: false, status: :not_found
+    end
+  end
+
+
+end
