21083454908f91adcd6b445b59d95a61ac3234ae
Quentin Gaultier <quentin.gaultier@gmail.com>
2020-05-05 23:52:16 +0200

    check email

--- Gemfile
+++ Gemfile
@@ -54,3 +54,5 @@ group :development do
 end
 
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+
+
--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -2,6 +2,6 @@ class AdminMailer < ApplicationMailer
 
   def otp_email(admin, code)
     @code = code
-    mail(to: admin.email, subject: "Your one time password")
+    mail(to: admin.email, subject: "Votre code REG")
   end
 end
