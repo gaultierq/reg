3f5f8d05e7141b8ff1a9a73ea6559a2d87e1a1e7
Quentin Gaultier <quentin.gaultier@gmail.com>
2020-06-25 20:10:43 +0200

    seeds++

--- db/seeds_prod.rb
+++ db/seeds_prod.rb
@@ -5,3 +5,6 @@ Admin.create(first_name: "Florent", last_name: "Beaurain", email: "beaurain.flor
 Admin.create(first_name: "Quentin", last_name: "Gaultier", email: "quentin.gaultier@gmail.com",
              phone_number: FFaker::PhoneNumberFR.unique.phone_number, password: "password", password_confirmation: "password", kind: "administrator")
 
+User.create(first_name: "Quentin", last_name: "Gaultier", email: "quentin.gaultier@gmail.com",
+             phone_number: FFaker::PhoneNumberFR.unique.phone_number, password: "password", password_confirmation: "password", kind: "client")
+
