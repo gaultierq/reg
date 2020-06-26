2d3fe614e0fed54c19937ba98ba7b46e085b496b
Quentin Gaultier <quentin.gaultier@gmail.com>
2020-06-26 20:05:13 +0200

    refix 98

--- app/models/tap_template.rb
+++ app/models/tap_template.rb
@@ -11,4 +11,7 @@ class TapTemplate < ApplicationRecord
   enum fluid_nature: { liquide: 0, gas: 1 }
   enum fluid_danger_group: { I: 0, II: 1 }, _prefix: true
   enum risk_category: { article_4_3_product: 0, I: 1, II: 2, III: 3, IV: 4 }, _prefix: true
+
+  validates :template_name, presence: true
+
 end
